2ca42dbf7e0387a80e0a3a693504a876e7bdc0e7
Letlotlo96 <mpeteletlotlo27@gmail.com>
2023-09-08 13:39:45 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,14 +3,15 @@
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a randomly generated number 
+ * main - Prints the last digit of a randomly generated number
  * and whether it is greater tan 5, less than 6, or 0.
  *
  * Return: Always 0.
- * */
+ */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
